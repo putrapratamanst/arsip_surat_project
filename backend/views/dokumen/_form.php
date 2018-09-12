b8e792f946ebadbbd5e8d76204ae1440cdf0c313
putrapratamanst <putra@docotel.com>
2018-09-12 23:27:54 +0700

    update dropdown tahun

--- backend/views/dokumen/_form.php
+++ backend/views/dokumen/_form.php
@@ -44,7 +44,29 @@ $listposisi=ArrayHelper::map($posisi,'id','posisi');
     ); ?>     
   
 
-           <?= $form->field($model, 'tahun')->textInput(['maxlength' => true]) ?>
+           <!-- $form->field($model, 'tahun')->textInput(['maxlength' => true]) -->
+           <?= $form->field($model, 'tahun')->dropDownList(
+                       [
+                        '2001' => '2001', 
+                        '2002' => '2002', 
+                        '2003' => '2003',
+                        '2004' => '2004',
+                        '2005' => '2005',
+                        '2006' => '2006',
+                        '2007' => '2007',
+                        '2008' => '2008',
+                        '2009' => '2009',
+                        '2010' => '2010',
+                        '2011' => '2011',
+                        '2012' => '2012',
+                        '2013' => '2013',
+                        '2014' => '2014',
+                        '2015' => '2015',
+                        '2016' => '2016',
+                        '2017' => '2017',
+                        '2018' => '2018',
+                    ],['prompt'=>'Pilih Tahun']
+               ); ?>
 
           <?= $form->field($model, 'file_url')->fileInput()->label('Upload File') ?>
 
